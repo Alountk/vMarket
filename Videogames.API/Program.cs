78692bc3a52c797879a5de494cca54508c654959
Raul Marchan Catala <alountk@gmail.com>
2025-11-24 00:20:01 +0100

    feat: Add various SVG assets to the web project and enable CORS in the API.

--- Videogames.API/Program.cs
+++ Videogames.API/Program.cs
@@ -13,6 +13,17 @@ builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddControllers();
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 // Add Infrastructure (and Application services via Infrastructure DI)
@@ -84,6 +95,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
